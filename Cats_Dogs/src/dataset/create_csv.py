9e579d396d0b7b8a1066dacf1f35c446738def50
Madeline Hawkins <madelinehawkins94@gmail.com>
2018-01-09 17:21:51 -0800

    More done to create_csv and started working on dataloader

--- Cats_Dogs/src/dataset/create_csv.py
+++ Cats_Dogs/src/dataset/create_csv.py
@@ -8,9 +8,15 @@ IMG_PATH = 'train/'
 dirs = os.listdir(IMG_PATH)
 
 with open('train/out.csv', 'wb') as csvfile:
+    animal = 'None'
     to_csv = csv.writer(csvfile, quoting=csv.QUOTE_NONE)
+    to_csv.writerow(['image_name', 'tag'])
     for file in dirs:
-        to_csv.writerow([file])
+        if 'dog' in file:
+            animal = 'dog'
+        else:
+            animal = 'cat'
+        to_csv.writerow([file, animal])
 
 
 
--- Cats_Dogs/src/dataset/dataloader.py
+++ Cats_Dogs/src/dataset/dataloader.py
@@ -1,4 +1,52 @@
-from torch.utils.data.dataset import Dataset
-from torchvision import transforms
+"""
+ Arguments:
+    A CSV file path
+    Path to image folder
+    PIL transforms
+
+"""
+
+
 import pandas as pd
+from torch import np
+
 import os
+from PIL import Image
+
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from sklearn.preprocessing import MultiLabelBinarizer
+
+IMG_PATH = 'train/'
+TRAIN_DATA = 'train/out.csv'
+
+class Cats_Dogs_Dataset(Dataset):
+
+    def __init__(self, csv_path, img_path, transform=None):
+
+        tmp_df = pd.read_csv(csv_path)
+        assert tmp_df['image_name'].apply(lambda x: os.path.isfile(img_path + x)).all()
+
+        self.mlb = MultiLabelBinarizer()
+        self.img_path = img_path
+        self.transform = transform
+
+        self.X_train = tmp_df['image_name']
+        self.y_train = self.mlb.fit_transform(tmp_df['tag'].str.split()).astype(np.float32)
+
+    def __getitem__(self, index):
+        img = Image.open(self.img_path + self.X_train[index])
+        img = img.convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+
+        label = torch.from_numpy(self.y_train[index])
+        return img, label
+
+    def __len__(self):
+        return len(self.X_train.index)
+
+
